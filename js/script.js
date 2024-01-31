7d6b130b791ff3381ddec9ed0bb1a45f53d08f02
vi_ser <vinceulisse@gmail.com>
2024-01-31 13:35:49 +0100

    logica funzionante

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*Prima di partire a scrivere codice poniamoci qualche domanda:*
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+*Consigli del giorno:*
+Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*BONUS 1:*
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+*BONUS 2:*
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+*/
+
+// conta dei numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    // controllo che il numero sia divisibile per 3
+    if (i % 3 == 0) {
+        //controllo che il numero sia divisibile ANCHE per 5
+        if (i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log("Fizz");
+        }
+
+    }
+    // controllo che il numero sia divisibile per 5
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+
+    else {
+        console.log(i);
+    }
+}
